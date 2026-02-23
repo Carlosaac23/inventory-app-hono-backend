9db0284f78fcc351e22da6c528717f1b4fce69dc
Carlos <carlosaac232001@gmail.com>
2026-02-23 12:25:14 -0500

    feat: create queries for edit and delete a car

--- src/config/queries.ts
+++ src/config/queries.ts
@@ -14,3 +14,14 @@ export async function addCar({
 }: Car) {
   await pg`INSERT INTO cars (car_model, car_brand, car_color, car_year, car_photo) VALUES (${car_model}, ${car_brand}, ${car_color}, ${car_year}, ${car_photo})`;
 }
+
+export async function editCar(
+  car_id: string,
+  { car_model, car_brand, car_color, car_year, car_photo }: Car,
+) {
+  await pg`UPDATE cars SET car_model = ${car_model}, car_brand = ${car_brand}, car_color = ${car_color}, car_year = ${car_year}, car_photo = ${car_photo} WHERE car_id = ${car_id}`;
+}
+
+export async function deleteCar(carID: string) {
+  await pg`DELETE FROM cars WHERE car_id = ${carID}`;
+}
