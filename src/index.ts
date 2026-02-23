0d60741476cac80298d748c0f193b707a88ad1bb
Carlos <carlosaac232001@gmail.com>
2026-02-22 19:45:06 -0500

    chore: import new function names

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { indexRoutes } from "./routes/indexRoutes.js";
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+
+import { routes } from './routes/index.js';
 
 const app = new Hono();
 
-app.use("*", cors());
-app.route("/", indexRoutes);
+app.use('*', cors());
+app.route('/', routes);
 
 export default app;
