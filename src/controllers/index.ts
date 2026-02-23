150df6bdd9eaa90e4ed2ffb23180440cc3918d82
Carlos <carlosaac232001@gmail.com>
2026-02-23 12:25:31 -0500

    feat: create controller for edit and delete car

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,20 +1,24 @@
 import type { Context } from 'hono';
 
-import { getAllCars, addCar } from '../config/queries.js';
+import type { Car } from '../types/index.js';
+
+import { getAllCars, addCar, editCar, deleteCar } from '../config/queries.js';
 
 export async function getAllCarsController(c: Context) {
   try {
-    const data = await getAllCars();
+    const allCars = await getAllCars();
 
-    if (!data) {
+    if (!allCars) {
       const error = new Error('Unable to load cars');
       c.status(500);
       return c.json({ msg: error.message });
     }
 
-    return c.json(data);
+    return c.json(allCars);
   } catch (error) {
     console.error('Controller error:', error);
+    c.status(500);
+    return c.json({ msg: 'Internal server error' });
   }
 }
 
@@ -25,5 +29,54 @@ export async function addCarController(c: Context) {
     return c.json({ msg: 'Car successfully added.' });
   } catch (error) {
     console.error('Controller error:', error);
+    c.status(400);
+    return c.json({ msg: 'Invalid request data' });
+  }
+}
+
+export async function editCarController(c: Context) {
+  try {
+    const body = await c.req.json();
+    const carID = c.req.param('id');
+
+    const allCars = await getAllCars();
+    const currentCar = allCars.find((car: Car) => car.car_id === carID);
+
+    if (!currentCar) {
+      const error = new Error('Car does not exist.');
+      c.status(404);
+      return c.json({ msg: error.message });
+    }
+
+    const updatedCar = { ...currentCar, ...body };
+
+    await editCar(carID, updatedCar);
+    return c.json({ msg: 'Car successfully updated.' });
+  } catch (error) {
+    console.error('Controller error:', error);
+    c.status(400);
+    return c.json({ msg: 'Invalid request data' });
+  }
+}
+
+export async function deleteCarController(c: Context) {
+  try {
+    const carID = c.req.param('id');
+
+    const allCars = await getAllCars();
+    const currentCar = allCars.find((car: Car) => car.car_id === carID);
+
+    if (!currentCar) {
+      const error = new Error('Car does not exist.');
+      c.status(404);
+      return c.json({ msg: error.message });
+    }
+
+    await deleteCar(carID);
+    return c.json({ msg: 'Car successfully deleted.' });
+  } catch (error) {
+    console.error('Controller error:', error);
+    c.status(500);
+    return c.json({ msg: 'Internal server error' });
   }
 }
