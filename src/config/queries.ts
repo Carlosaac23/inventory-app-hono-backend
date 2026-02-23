dde58a8c5998f317b1c09e7e4949890b618eaaa3
Carlos <carlosaac232001@gmail.com>
2026-02-22 19:45:50 -0500

    feat: delete unuse query functions and create new ones to project

--- src/config/queries.ts
+++ src/config/queries.ts
@@ -1,13 +1,16 @@
+import type { Car } from '../types/index.js';
+
 import { pg } from './db.js';
-
-export async function getUsers() {
-  return await pg`SELECT * FROM users`;
+export async function getAllCars() {
+  return await pg`SELECT * FROM cars`;
 }
 
-export async function addUser(name: string, surname: string, age: number) {
-  await pg`INSERT INTO users (name, surname, age) VALUES (${name}, ${surname}, ${age})`;
-}
-
-export async function deleteUser(userID: string) {
-  await pg`DELETE FROM users WHERE id = ${userID}`;
+export async function addCar({
+  car_model,
+  car_brand,
+  car_color,
+  car_year,
+  car_photo,
+}: Car) {
+  await pg`INSERT INTO cars (car_model, car_brand, car_color, car_year, car_photo) VALUES (${car_model}, ${car_brand}, ${car_color}, ${car_year}, ${car_photo})`;
 }
